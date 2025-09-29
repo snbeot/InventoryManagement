23f3c90a25629d9adcf4c83ce43168f992b518fc
San <snbeot25@gmail.com>
2025-09-28 20:43:30 -0500

    Add project files.

---
+++ InventoryManagement/Controllers/PlaylistController.cs
@@ -0,0 +1,37 @@
+﻿using InventoryManagement.Models;
+using InventoryManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class PlaylistController: Controller
+    {
+        private readonly MongoDBService _mongoDBService;
+        [HttpGet]
+        public async Task<List<Playlist>> Get()
+        {
+            return await _mongoDBService.GetAsync();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Playlist playlist)
+        {
+            await _mongoDBService.CreateAsync(playlist);
+            return CreatedAtAction(nameof(Get), new { id = playlist.Id }, playlist);
+        }
+        [HttpPut("id")]
+        public async Task<IActionResult> AddToPlaylist(string id, [FromBody] string movieId)
+        {
+            await _mongoDBService.AddTpPlaylistAsync(id, movieId);
+            return NoContent();
+        }
+        [HttpDelete("id")]
+        public async Task<IActionResult> Delete(String id)
+        {
+            await _mongoDBService.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ InventoryManagement/Models/Playlist.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace InventoryManagement.Models
+{
+    public class Playlist
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string username { get; set; } = null!;
+        [BsonElement("items")]
+        [JsonPropertyName("items")]
+        public List<string> movieIds { get; set; } = null!;
+    }
+}
